93a0575e37a6e64b49413d8a29f4e39eda0d17b2
mocha <>
2018-01-10 17:47:41 +0900

    devise自体の日本語化を記述

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    #日本語にする
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,5 +22,6 @@ module ChatSpace
       g.test_framework false
     end
     #ここまで
+
   end
 end
