e314c5ee406bbc2ae1c30b7cda55a29c67546317
mocha <>
2018-01-16 15:59:32 +0900

    テストコードの大枠を作成

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,13 @@
-FactoryGirl.define do
-  factory :message do
-    content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
-    user
-    group
+require 'rails_helper'
+
+RSpec.describe Message, type: :model do
+  describe '#create' do
+    context 'can save' do
+　# この中にメッセージを保存できる場合のテストを記述
+    end
+
+    context 'can not save' do
+　# この中にメッセージを保存できない場合のテストを記述
+    end
   end
 end
