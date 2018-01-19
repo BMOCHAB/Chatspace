05ee5cc26c21acb1eef5f398e15d5f1d7b5b17bf
mocha <>
2018-01-19 11:02:41 +0900

    条件分岐を記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,22 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @jsmessages = Message.all
+    @jsmessagesa = @jsmessages.last
+    respond_to do |format| 
+      format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
+      format.json #{ @new_message = Message.where('id > ?', params[:message][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
+    end
   end
+
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path(params[:group_id])  }
+        format.html { redirect_to group_messages_path(params[:group_id])} 
         format.json
-    end
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
